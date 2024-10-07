5a8c88b2b2accef36bda44031cc3784f5182bca3
davide.sforza.s1_giu <davide.sforza.s1_giu@LABGIOVEP1>
2024-10-07 08:30:19 +0200

    Aggiungere i file di progetto.

---
+++ BlazorAppAntiquariato/Models/Stand.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorAppAntiquariato.Models
+{
+    public class Stand
+    {
+        public int idStand { get; set; }
+        public string? nome { get; set; }
+        public string? cognome { get; set; }
+        public char zona { get; set; }
+        public byte superficie { get; set; }
+        public bool isCoperto { get; set; }
+        public DateTime dataInizio { get; set; }
+    }
+}
