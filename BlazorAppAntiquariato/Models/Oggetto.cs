3dcf09c1afaafa16fc7eb0ea494beae0967ba0e7
davide.sforza.s1_giu <davide.sforza.s1_giu@LABGIOVEP1>
2024-10-12 11:22:19 +0200

    Modifiche al 07-10

---
+++ BlazorAppAntiquariato/Models/Oggetto.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorAppAntiquariato.Models
+{
+    public class Oggetto
+    {
+        public int idOggetto { get; set; }
+        public string? tipo { get; set; }
+        public string? materialePrincipale { get; set; }
+        public string? epoca { get; set; }
+        public decimal prezzoEsposto { get; set; }
+        public int standId { get; set; }
+    }
+}
